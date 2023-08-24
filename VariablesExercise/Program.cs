14867205df381349fb80e1137fa999b126d8494f
Graham Spencer <gspencer447@yahoo.com>
2023-08-24 14:36:23 -0500

    completed the exercise and declared/initialized all variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Graham Spencer";
+            int myAge = 28;
+            char firstLetter = 'G';
+            bool isCoding = true;
+            double monthlyFee = 14.99;
+            decimal hoursPlayed = 948.88357m;
+
+            Console.WriteLine($"My name is {myName}. I am {myAge} years old and my name starts with the letter {firstLetter}. \n" +
+                $"Right now, it is {isCoding} that I am taking a coding course. \n" +
+                $"In my free time, I play a game that has a monthly subscription fee of ${monthlyFee}. \n" +
+                $"Over my lifetime, I have accumulated {hoursPlayed} hours of playtime.");
         }
     }
 }
